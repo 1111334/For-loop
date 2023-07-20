e8d3bdc15d3f692eea0fd409f7e71ad310efb1b2
Mike <109431533+1111334@users.noreply.github.com>
2023-07-20 10:56:59 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> ourNumbers = new ArrayList<>();
 
         for (int number = 5; number < 26; number++) {
             System.out.println(number);
